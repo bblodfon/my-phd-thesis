326fa73ecb250280dc146970f5a1b6e88626d0bd
john <bblodfon@gmail.com>
2019-12-03 14:59:33 +0100

    renamed build file

--- build-thesis.sh
+++ build-thesis.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')"
