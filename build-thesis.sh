872b9889a303505a74611cc1552d5fd82d0c2296
john <bblodfon@gmail.com>
2021-04-25 17:03:26 +0200

    remove previous text, compile new

--- build-thesis.sh
+++
@@ -1,3 +0,0 @@
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')"
