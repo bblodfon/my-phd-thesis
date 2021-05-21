d68567b64ffe9797941ace2471cd8c7460cea9de
john <bblodfon@gmail.com>
2021-05-21 14:18:38 +0200

    update page numbering

--- replace_pdf.sh
+++ replace_pdf.sh
@@ -6,15 +6,15 @@ set -x
 thesis="docs/phd-thesis.pdf"
 
 # page number that has the "PAPER 1" title in the thesis (before the UBDs paper)
-paper1_page=36
+paper1_page=46
 paper2_page=$(($paper1_page+4))
-
+paper3_page=$(($paper2_page+7))
 
 # keep bookmark info from thesis pdf
 pdftk docs/phd-thesis.pdf dump_data > thesis.info
 
 # merge pdfs
-pdftk T=docs/phd-thesis.pdf A=papers/ubds.pdf B=papers/pubdictionaries.pdf C=papers/emba.pdf D=papers/bias.pdf cat T1-$paper1_page A T$paper2_page B T47 C T53 D T84 output out.pdf
+pdftk T=docs/phd-thesis.pdf A=papers/ubds.pdf B=papers/pubdictionaries.pdf C=papers/emba.pdf D=papers/bias.pdf cat T1-$paper1_page A T$paper2_page B T$paper3_page C T63 D T94 output out.pdf
 
 # retain original outline
 pdftk out.pdf update_info thesis.info output out2.pdf
