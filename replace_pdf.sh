f2c9664be54a55615dc82671e57ec4ed8f9c7285
john <bblodfon@gmail.com>
2021-05-24 00:07:41 +0200

    fix page numbering and pdftk command

--- replace_pdf.sh
+++ replace_pdf.sh
@@ -6,15 +6,18 @@ set -x
 thesis="docs/phd-thesis.pdf"
 
 # page number that has the "PAPER 1" title in the thesis (before the UBDs paper)
-paper1_page=46
+paper1_page=58
 paper2_page=$(($paper1_page+4))
 paper3_page=$(($paper2_page+7))
+paper4_page=$(($paper3_page+41))
+paper5_page=$(($paper4_page+6))
+last_page=$(($paper5_page+31))
 
 # keep bookmark info from thesis pdf
 pdftk docs/phd-thesis.pdf dump_data > thesis.info
 
 # merge pdfs
-pdftk T=docs/phd-thesis.pdf A=papers/ubds.pdf B=papers/pubdictionaries.pdf C=papers/emba.pdf D=papers/bias.pdf cat T1-$paper1_page A T$paper2_page B T$paper3_page C T63 D T94 output out.pdf
+pdftk T=docs/phd-thesis.pdf A=papers/ubds.pdf B=papers/pubdictionaries.pdf C=papers/ags.pdf D=papers/emba.pdf E=papers/bias.pdf cat T1-$paper1_page A T$paper2_page B T$paper3_page C T$paper4_page D T$paper5_page E T$last_page output out.pdf
 
 # retain original outline
 pdftk out.pdf update_info thesis.info output out2.pdf
